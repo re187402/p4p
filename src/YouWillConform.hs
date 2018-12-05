56c8fe8f9341d80e782020292891ede30b900643
Rena Enami <renaenami.0311@gmail.com>
2018-12-05 10:55:05 +0900

    update

--- src/YouWillConform.hs
+++ src/YouWillConform.hs
@@ -9,34 +9,54 @@ type Cap = Char
 type Cmd = String
 
 type Pos = Int
+type Range = (Pos,Pos)
 
 --
 
 cap1 :: [Cap]
 cap1 = "FFBBBFBBBFFBF"
 
+ranges1 :: [Range]
+ranges1 = [(3, 5), (7, 9), (12, 12)]
+
 cap2 :: [Cap]
 cap2 = "FFBBBFBBBFFFF"
 
-
 --
-pleaseConform :: [Cap] -> [Cmd]
-pleaseConform = map mkCmd . gonyogonyo
 
-mkCmd ::(Pos,Pos) -> Cmd
-mkCmd  (i, j ) = showPos i++"から"
-             ++  showPos j++"の人は帽子の向きを変えてください"
-             
+pleaseConform :: [Cap] -> [Cmd]
+pleaseConform = map mkCmd . takeEvery2nd . gonyogonyo
+
+
+makeCommands :: [Range] -> [Cmd]
+makeCommands = map mkCmd
+
+mkCmd :: Range -> Cmd
+mkCmd  (i, j )
+  |i==j = showPos i ++"の人は帽子の向きを変えてください"
+  |i/=j = showPos i ++"から"
+      ++  showPos j ++"の人は帽子の向きを変えてください"
+
+cmdStr :: Cmd
+cmdStr = "の人は帽子の向きを変えてください"
+
 showPos pos = show pos ++"番目"
-gonyogonyo :: [Cap] ->[(Pos, Pos)]
+
+takeEvery2nd :: [a] -> [a]
+takeEvery2nd []  = []
+takeEvery2nd (x:[]) = []
+takeEvery2nd (x:(y:zs)) = y : takeEvery2nd zs
+
+
+gonyogonyo :: [Cap] ->[Range]
 gonyogonyo = undefined
+
 
 --
 
 sample :: [Int]
-sample = [1,1,2,3,5,8,13]
+sample = [1,1,2,3]
 
 twoplus :: Int -> Int
 twoplus n = n + 2
 
-hoge = map twoplus
